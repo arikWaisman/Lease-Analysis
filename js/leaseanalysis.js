f6195ea1d96ab5f8143a97d603c6bd88faf93618
Arik Waisman <arik.waisman@gmail.com>
2016-01-17 18:39:53 -0500

    Re-worked error notification to no longer use alert function

--- js/leaseanalysis.js
+++ js/leaseanalysis.js
@@ -1,20 +1,18 @@
 
 function calculate(){
 
-var sticker = parseFloat(document.getElementById("sticker").value, 10);
-var invoice = parseFloat(document.getElementById("invoice").value, 10);
-var residual = parseFloat(document.getElementById("residual").value, 10);
-var residualValue = sticker * residual;
-var dprAmount = invoice - residualValue;
-var term = parseFloat(document.getElementById("term").value, 10);
-var dprPayment = dprAmount / term;
-var interest = parseFloat(document.getElementById("interest").value, 10);
-var intPayment = (invoice - residualValue) * interest;
-var paymentBeforeTax = dprPayment + intPayment;
-var tax = parseFloat(document.getElementById("tax").value, 10);
-var lpdoAmount = (paymentBeforeTax * tax) + paymentBeforeTax;
-//var validation = [sticker, invoice, residual, term, interest, tax];
-		
+	var sticker = parseFloat(document.getElementById("sticker").value, 10);
+	var invoice = parseFloat(document.getElementById("invoice").value, 10);
+	var residual = parseFloat(document.getElementById("residual").value, 10);
+	var residualValue = sticker * residual;
+	var dprAmount = invoice - residualValue;
+	var term = parseFloat(document.getElementById("term").value, 10);
+	var dprPayment = dprAmount / term;
+	var interest = parseFloat(document.getElementById("interest").value, 10);
+	var intPayment = (invoice - residualValue) * interest;
+	var paymentBeforeTax = dprPayment + intPayment;
+	var tax = parseFloat(document.getElementById("tax").value, 10);
+	var lpdoAmount = (paymentBeforeTax * tax) + paymentBeforeTax;		
 
 	console.log(residualValue);
 	document.getElementById("residualvalue").textContent = 
@@ -27,7 +25,7 @@ var lpdoAmount = (paymentBeforeTax * tax) + paymentBeforeTax;
 	console.log(dprPayment);
 	document.getElementById("dprpayment").textContent = 
 	"$" + parseFloat(Math.round(dprPayment * 100) / 100).toFixed(2);
-	
+
 	console.log(intPayment);
 	document.getElementById("intpayment").textContent = 
 	"$" + parseFloat(Math.round(intPayment * 100) / 100).toFixed(2);
@@ -39,17 +37,24 @@ var lpdoAmount = (paymentBeforeTax * tax) + paymentBeforeTax;
 	console.log(lpdoAmount);
 	document.getElementById("lowestdriveoffamount").textContent = 
 	"$" + parseFloat(Math.round(lpdoAmount * 100) / 100).toFixed(2);
+
 };
 
 function validate(){
-var sticker = parseFloat(document.getElementById("sticker").value, 10);
-var invoice = parseFloat(document.getElementById("invoice").value, 10);
-var residual = parseFloat(document.getElementById("residual").value, 10);
-var term = parseFloat(document.getElementById("term").value, 10);		
-var interest = parseFloat(document.getElementById("interest").value, 10);
-var tax = parseFloat(document.getElementById("tax").value, 10);
-var validation = [];
-		
+
+	//variables for input validation
+	var sticker = parseFloat(document.getElementById("sticker").value, 10);
+	var invoice = parseFloat(document.getElementById("invoice").value, 10);
+	var residual = parseFloat(document.getElementById("residual").value, 10);
+	var term = parseFloat(document.getElementById("term").value, 10);		
+	var interest = parseFloat(document.getElementById("interest").value, 10);
+	var tax = parseFloat(document.getElementById("tax").value, 10);
+	var validation = [];
+
+	//variables for error dev
+	var body = document.querySelector("body");
+	var errorDiv = document.getElementsByClassName("error")[0];
+			
 
 	if (!sticker){
 		validation.push(document.getElementById('sticker'));
@@ -86,15 +91,32 @@ var validation = [];
 	} else {
 		document.getElementById("tax").className = "alert alert-success pull-right";
 	}
-	
-	if (validation.length == 0){
 
+	if (validation.length == 0){
+		if(document.getElementsByClassName("error").length == 1){
+			body.removeChild(errorDiv);
+		}
 		calculate();
 	} else {
 		for(i = 0; i < validation.length; i++){
 			validation[i].className = "alert alert-danger pull-right";
 		}
-		alert("all fields must be filled out and be numbers (no commas)");
+		if(document.getElementsByClassName("error").length == 0){
+			errorElement();	
+		}
 	}
+
+	function errorElement(){
+
+		var section = document.querySelector("section");
+		var errorElement = document.createElement("div")
+		var errorText = document.createTextNode("all fields must be filled out and be numbers (no commas)");
+
+		errorElement.appendChild(errorText);
+		errorElement.className = "container error";
+		body.insertBefore(errorElement, section);
+
+	}
+	
 };
 	
